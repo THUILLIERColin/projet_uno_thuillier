822201421bbcb833a13fb95783f3d15a1eb23274
camille <czell57890@gmail.com>
2022-05-05 11:21:45 +0200

    prout
    Expert modif + creation

--- ProjetUNO/src/expert/ExpertCartePlus2CarteSimple.java
+++ ProjetUNO/src/expert/ExpertCartePlus2CarteSimple.java
@@ -1,32 +1,31 @@
 package expert;
 
 import cartes.Carte;
-import cartes.CartePasser;
+import cartes.Plus2;
 import cartes.CarteSimple;
 import partie.Partie;
 
-public class ExpertCartePasserCarteSimple extends Expert{
+public class ExpertCartePlus2CarteSimple extends Expert{
 
-    public ExpertCartePasserCarteSimple(Expert suivant){
+    public ExpertCartePlus2CarteSimple(Expert suivant){
         super(suivant);
     }
 
     @Override
     public boolean expertise(Carte carte) throws Exception {
         Partie partie = Partie.getInstance();
-
         if (carte.getCouleur() == super.getPartie().getPremiereCarteTas().getCouleur()){
             return true;
         }
-
         return false;
     }
 
     @Override
     public boolean analyse(Carte carte) {
-        if(carte instanceof CartePasser && super.getPartie().getPremiereCarteTas() instanceof CarteSimple
-                || carte instanceof CarteSimple && super.getPartie().getPremiereCarteTas() instanceof CartePasser)
+        if(carte instanceof Plus2 && super.getPartie().getPremiereCarteTas() instanceof CarteSimple
+                || carte instanceof CarteSimple && super.getPartie().getPremiereCarteTas() instanceof Plus2)
             return true;
         return false;
     }
 }
+
