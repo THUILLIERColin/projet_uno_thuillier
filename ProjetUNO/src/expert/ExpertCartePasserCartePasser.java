85972526983c2e41d5a4bb2afc2210096e4412fd
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2022-05-05 10:26:04 +0200

    ExpertCartePasser

--- ProjetUNO/src/expert/ExpertCartePasserCartePasser.java
+++ ProjetUNO/src/expert/ExpertCartePasserCartePasser.java
@@ -1,19 +1,22 @@
 package expert;
 
 import cartes.Carte;
+import cartes.CartePasser;
+import partie.Partie;
 
 public class ExpertCartePasserCartePasser extends Expert{
 
     public ExpertCartePasserCartePasser(Expert suivant){ super(suivant);}
 
     @Override
-    public boolean expertise(Carte cartes) throws Exception {
-        return false;
+    public boolean expertise(Carte carte) throws Exception {
+        return true;
     }
 
     @Override
-    public boolean analyse(Carte cartes) {
-        if()
+    public boolean analyse(Carte carte) {
+        if(carte instanceof CartePasser && Partie.getInstance().getPremiereCarteTas() instanceof CartePasser)
+            return true;
         return false;
     }
 }
