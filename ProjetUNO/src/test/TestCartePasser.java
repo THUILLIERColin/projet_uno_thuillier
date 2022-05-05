f2209228401fa3704856751f654866f9e55937fd
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2022-05-05 11:13:59 +0200

    Update TestCartePasser

--- ProjetUNO/src/test/TestCartePasser.java
+++ ProjetUNO/src/test/TestCartePasser.java
@@ -27,7 +27,7 @@ public class TestCartePasser {
 
             /*
                     TEST 1 : PASSER
-             
+
 
             System.out.println("\n---------------------------------\nTEST 1 : Passer\n");
 
@@ -52,26 +52,16 @@ public class TestCartePasser {
                 System.out.println("La premiere carte est :" + partie.getPremiereCarteTas());
 
             }catch (Exception e){
-                System.out.println(e);
+                System.out.println("\nATTENTION " + e.getMessage()+"\n");
             }
 
             /*
                     TEST 2 : CARTE SIMPLE ILLEGALE
-             */
+
 
             System.out.println("\n---------------------------------\nTEST 2 : CARTE SIMPLE ILLEGALE\n\n");
 
             System.out.println("Le joueur courant est "+ partie.getJoueurCourant());
-
-            /*
-            Vérifier que Alice est bien le joueur courant
-            Alice pose le « Passe ton tout rouge »
-            Alice finit son tour
-            Vérifier que Charles est le joueur courant Vérifier que Charles possède bien 3 cartes
-            Charles pose le « 1 Bleu »
-            Charles finit son tour
-            Vérifier dans l’exception appropriée que Charles a toujours 3 cartes
-             */
 
             try{
                 partie.getJoueurCourant().jouer(partie.getJoueurCourant().getCarte(0));
@@ -83,10 +73,35 @@ public class TestCartePasser {
                 partie.getJoueurCourant().jouer(partie.getJoueurCourant().getCarte(0));
                 partie.getJoueurCourant().finirTour();
             }catch (Exception e){
-                System.out.println(e);
+                System.out.println("\nATTENTION " + e.getMessage()+"\n");
                 System.out.println(""+ partie.getJoueurCourant()+" possède : "+ partie.getJoueurCourant().TailleDeLaMain()+" cartes");
             }
 
+            /*
+                    TEST 2 : CARTE PASSE TOUR ILLEGALE
+             */
+
+            System.out.println("\n---------------------------------\nTEST 2 : CARTE PASSE TOUR ILLEGALE\n\n");
+
+            System.out.println("Le joueur courant est "+ partie.getJoueurCourant());
+
+            try{
+                partie.getJoueurCourant().jouer(partie.getJoueurCourant().getCarte(1));
+                partie.getJoueurCourant().finirTour();
+
+                System.out.println("");
+                System.out.println("Le joueur courant est "+ partie.getJoueurCourant());
+                partie.getJoueurCourant().jouer(partie.getJoueurCourant().getCarte(2));
+                partie.getJoueurCourant().finirTour();
+
+                System.out.println("Le joueur courant est "+ partie.getJoueurCourant());
+                System.out.println(""+ partie.getJoueurCourant()+" possède : "+ partie.getJoueurCourant().TailleDeLaMain()+" cartes");
+                partie.getJoueurCourant().jouer(partie.getJoueurCourant().getCarte(1));
+                partie.getJoueurCourant().finirTour();
+            }catch (Exception e){
+                System.out.println("\nATTENTION " + e.getMessage()+"\n");
+                System.out.println(""+ partie.getJoueurCourant()+" possède : "+ partie.getJoueurCourant().TailleDeLaMain()+" cartes");
+            }
 
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
