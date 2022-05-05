894189433343f460b03fb53148f3ae157cf91fb0
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2022-05-05 10:00:30 +0200

    Modify Expert

--- ProjetUNO/src/expert/ExpertCartePasserSurCarteSimple.java
+++ ProjetUNO/src/expert/ExpertCartePasserSurCarteSimple.java
@@ -1,4 +1,32 @@
 package expert;
 
-public class ExpertCartePasserSurCarteSimple {
+import cartes.Carte;
+import cartes.CartePasser;
+import cartes.CarteSimple;
+import exceptions.UnoException;
+import partie.Partie;
+
+public class ExpertCartePasserSurCarteSimple extends Expert{
+
+    public ExpertCartePasserSurCarteSimple(Expert suivant){
+        super(suivant);
+    }
+
+    @Override
+    public boolean expertise(Carte carte) throws Exception {
+        Partie partie = Partie.getInstance();
+
+        CartePasser carteJoueur = (CartePasser) carte;
+        CarteSimple carteTas = (CarteSimple) super.getPartie().getPremiereCarteTas();
+        if ((carte.getCouleur() == super.getPartie().getPremiereCarteTas().getCouleur()))
+            return true;
+        return false;
+    }
+
+    @Override
+    public boolean analyse(Carte cartes) {
+        if(cartes instanceof CartePasser && super.getPartie().getPremiereCarteTas() instanceof CarteSimple)
+            return true;
+        return false;
+    }
 }
--- ProjetUNO/src/expert/ExpertCarteSimpleSurCarteSimple.java
+++ ProjetUNO/src/expert/ExpertCarteSimpleSurCarteSimple.java
@@ -11,37 +11,13 @@ public class ExpertCarteSimpleSurCarteSimple extends Expert{
         super(suivant);
     }
 
-    /*                                    /
-            A VERIFIER AVEC EXPERT.JAVA \/
-     */
-
-    /*
-            ICI IL FAUT COMPARER LA COULEUR ET L'ENTIER
-     */
-
     @Override
     public boolean expertise(Carte carte) throws Exception {
 
-        /*
-                    ATTENTION UNE CARTE SIMPLE PEU ETRE POSÉ SUR TOUTES LES AUTRE CARTES
-
-            Si la carte est ChangerCouleur / Plus2 / Reverse
-                Alors verifier la couleur et si on est bien le joueur qui doit jouer
-                Sinon pénalité
-
-            Si c'est une carte simple
-
-            --> faire dans un autre
-            -->
-         */
         Partie partie = Partie.getInstance();
 
         CarteSimple carteJoueur = (CarteSimple) carte;
         CarteSimple carteTas = (CarteSimple) super.getPartie().getPremiereCarteTas();
-        if (partie.getJoueurCourant().doitDireUno() && !partie.getJoueurCourant().getUno())
-            throw new UnoException("Ce joueur possede une carte et n'a pas dit UNO", partie.getJoueurCourant());
-        if (partie.getJoueurCourant().doitDireUno() && partie.getJoueurCourant().getUno())
-            throw new UnoException("", partie.getJoueurCourant());
         if ((carte.getCouleur() == super.getPartie().getPremiereCarteTas().getCouleur()) || (carteJoueur.getNumero() == carteTas.getNumero()))
             return true;
                 // C'est un class Carte et il n'existe pas de getNumero() dedans, il faudrait convertir en CarteSimple
