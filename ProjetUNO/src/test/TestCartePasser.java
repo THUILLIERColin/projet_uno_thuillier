cb4c0a93755a27e49765287d8a92b9fe3c0799fe
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2022-05-05 10:18:44 +0200

    Delete remark

--- ProjetUNO/src/test/TestCartePasser.java
+++ ProjetUNO/src/test/TestCartePasser.java
@@ -1,4 +1,69 @@
 package test;
 
+import expert.ExpertCarteSimpleCarteSimple;
+import fichiers.Fichier;
+import fichiers.Parser;
+import fichiers.ParserCarteSimple;
+import partie.Partie;
+
 public class TestCartePasser {
+    public static void main(String[] args) {
+        try {
+            Partie partie = Partie.getInstance();
+
+            String nomDuFichier = "/JeuTestCarteSimple.csv";
+            nomDuFichier = Fichier.class.getResource(nomDuFichier).getPath();
+
+            Parser premierParser = new ParserCarteSimple(null);
+
+            Fichier.lire(nomDuFichier, premierParser);
+
+            partie.setExpert(new ExpertCarteSimpleCarteSimple(null));
+
+            partie.initialisationPartie(3);
+
+            /*
+                    TEST 1 : ALICE
+             */
+
+            System.out.println("Le joueur courant est "+ partie.getJoueurCourant());
+            System.out.println(""+partie.getJoueurCourant()+" possede "+ partie.getJoueurCourant().TailleDeLaMain()+" cartes");
+
+
+            try{
+                partie.getJoueurCourant().jouer(partie.getJoueurCourant().getCarte(0));
+                System.out.println(""+partie.getJoueurCourant()+" possède : "+ partie.getJoueurCourant().TailleDeLaMain()+" cartes");
+                System.out.println("Alice a la main : " + partie.getJoueurCourant().getLaMain());
+                System.out.println("La premiere carte est :" + partie.getPremiereCarteTas());
+                System.out.println("Le nombre de cartes du tas est "+ partie.getTailleTas());
+                partie.getJoueurCourant().finirTour();
+                System.out.println("C'est le tour de "+ partie.getJoueurCourant());
+
+            }catch (Exception e){
+                System.out.println(e);
+            }
+
+            /*
+                    TEST 2 : BOB
+             */
+
+            System.out.println("\n---------------------------------\nTEST 2 : BOB\n");
+            System.out.println("\n"+partie.getJoueurCourant()+" possède : "+ partie.getJoueurCourant().TailleDeLaMain()+" cartes");
+
+            try{
+                partie.getJoueurCourant().jouer(partie.getJoueurCourant().getCarte(0));
+                System.out.println("Bob possède : "+ partie.getJoueurCourant().TailleDeLaMain()+" cartes");
+                System.out.println("Bob a la main : " + partie.getJoueurCourant().getLaMain());
+                System.out.println("La premiere carte est :" + partie.getPremiereCarteTas());
+                System.out.println("Le nombre de cartes du tas est "+ partie.getTailleTas());
+                partie.getJoueurCourant().finirTour();
+                System.out.println("C'est le tour de "+ partie.getJoueurCourant());
+            }catch (Exception e){
+                System.out.println(e);
+            }
+
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
--- ProjetUNO/src/test/TestCarteSimple.java
+++ ProjetUNO/src/test/TestCarteSimple.java
@@ -13,28 +13,10 @@ public class TestCarteSimple {
 
             String nomDuFichier = "/JeuTestCarteSimple.csv";
             nomDuFichier = Fichier.class.getResource(nomDuFichier).getPath();
-            // Maintenant, à vous de jouer !
 
             Parser premierParser = new ParserCarteSimple(null);
 
-			/* Ou bien
-
-			Parser premierParser =null;
-			premierParser =new ParserCaseDepart(premierParser);
-			premierParser = new ParserGare(premierParser);
-
-			 */
-
-
-
-            // A vous de créer des parser puis de les chainer les uns aux autres avant d'envoyer
-            // le premier à la méthode lire
-
             Fichier.lire(nomDuFichier, premierParser);
-
-            /*
-                    TEST AFFICHAGE CARTE : OK
-             */
 
             partie.setExpert(new ExpertCarteSimpleCarteSimple(null));
 
@@ -43,6 +25,7 @@ public class TestCarteSimple {
             /*
                     TEST 1 : ALICE
              */
+
             System.out.println("Le joueur courant est "+ partie.getJoueurCourant());
             System.out.println(""+partie.getJoueurCourant()+" possede "+ partie.getJoueurCourant().TailleDeLaMain()+" cartes");
 
@@ -59,8 +42,6 @@ public class TestCarteSimple {
             }catch (Exception e){
                 System.out.println(e);
             }
-
-
 
 
             /*
