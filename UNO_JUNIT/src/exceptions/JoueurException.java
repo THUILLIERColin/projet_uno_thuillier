3a5189c504b5e4037174bd7ab7f7739d2dc20d79
camille <czell57890@gmail.com>
2022-06-03 15:40:15 +0200

    Exceptions
    Commentaire exeptions

--- UNO_JUNIT/src/exceptions/JoueurException.java
+++ UNO_JUNIT/src/exceptions/JoueurException.java
@@ -5,6 +5,11 @@ import joueur.Joueur;
 public class JoueurException extends Exception{
     Joueur mauvaisJoueur;
 
+    /**
+     * Exception lorsqu'un joueur joue au mauvais moment
+     * @param msg message a afficher
+     * @param mauvaisJoueur joueur qui provoque l'erreur
+     */
     public JoueurException(String msg, Joueur mauvaisJoueur){
         super(msg);
         this.mauvaisJoueur=mauvaisJoueur;
